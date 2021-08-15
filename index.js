b9fc220109a6870dcdf584a1218bf5752c80f379
Vitória Santos <vitoria.fer.santos@hotmail.com>
2021-08-15 00:04:51 -0300

    Commit html e css do site-teste

---
+++ index.js
@@ -0,0 +1,12 @@
+// Isso é um comentário
+/* mais 
+linhas
+para comentar
+Pars fechar siga o comando */ 
+console.log ("Hello World")
+const variavel = 10
+let outraVariavel = 20
+
+console.log (variavel, outraVariavel)
+let soma = variavel + outraVariavel
+console.log (" a soma é ", soma)
